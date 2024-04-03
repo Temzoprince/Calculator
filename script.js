39e1c7552d09311adc31dbe9b687f1ada442cce6
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-04-03 12:21:48 +0100

    Add operate function
    Operate function takes in the two numbers and the operator and performs
    the calculation returning the result

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+Create a Calculator
+*/
+
+// Calculator operations
+
 const add = (a,b) => a + b
 
 const subtract = (a,b) => a - b
@@ -5,3 +11,23 @@ const subtract = (a,b) => a - b
 const multiply = (a,b) => a * b
 
 const divide = (a, b) => a / b
+
+let num1;
+let num2;
+let operator;
+
+//Operate function
+
+const operate = (num1, num2, operator) => {
+    if (operator == '+') {
+        return add(num1, num2)
+    } else if (operator == '-') {
+        return subtract(num1, num2)
+    } else if(operator == '*') {
+        return multiply(num1, num2)
+    } else if(operator == '/') {
+        return divide(num1, num2)
+    }
+}
+
+console.log(operate(3, 4, '*'))
