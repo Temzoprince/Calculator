b9460758b67f6201126431df2aa5794023a51bed
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-04-04 22:29:01 +0100

    Complete calculator layout

--- script.js
+++ script.js
@@ -6,81 +6,7 @@ Create a Calculator
  * Dom manipulation for On Screen Calculator
  */
 
-// Calculator Buttons array
-const buttonsArr = [
-  "7",
-  "4",
-  "1",
-  "0",
-  "8",
-  "5",
-  "2",
-  ".",
-  "9",
-  "6",
-  "3",
-  "=",
-  "/",
-  "*",
-  "-",
-  "+",
-];
-
-//Function to create grid
-function createGrid(gridSize, element) {
-  const grid = document.querySelector(element);
-  for (i = 1, arr = 0; i <= gridSize; i++) {
-    let col = document.createElement("div");
-    col.classList.add(`grid-column-${i}`);
-    for (j = 1; j <= gridSize; j++) {
-      let row = document.createElement("div");
-      row.classList.add(`grid-row-${j}`);
-      let btn = document.createElement("btn");
-      btn.classList.add(`btn-${buttonsArr[arr]}`)
-      btn.textContent = `${buttonsArr[arr]}`;
-      row.appendChild(btn);
-      arr++;
-      col.appendChild(row);
-    }
-    grid.appendChild(col);
-  }
-}
-
-// Function to display text on calculator
-displayTextSpan = document.querySelector(".display-text");
-let addTextToDisplay = function (text, update) {
-  if (update) {
-    return displayTextSpan.textContent += text;
-  }
-  return displayTextSpan.textContent = text;
-};
-
-// Function to populate display when buttons clicked
-let numberButtonsUpdateDisplay = function(btnNumber) {
-  let number = document.querySelector(`.btn-${btnNumber}`)
-
-  number.addEventListener('click', () => {
-    if (displayTextSpan.textContent == ans) {
-      displayValue = addTextToDisplay(`${btnNumber}`, false)
-      console.log(displayValue)
-    } else {
-      displayValue = addTextToDisplay(`${btnNumber}`, true)
-      console.log(displayValue)
-    }
-  })
-}
-
-let addBtnCallAdd = function() {
-  let addBtn = document.querySelector(`.btn-+`)
-  addBtn.addEventListener('click', () => {
-    num1 = displayTextSpan.textContent
-    displayTextSpan.textContent = '';
-    displayValue = addTextToDisplay(`${btnNumber}`, true)
-    console.log(num1)
-    console.log(displayValue)
-  })
-}
-
+//
 
 /*
  * Functionality of Calculator
@@ -124,21 +50,6 @@ const operate = (num1, num2, operator) => {
   }
 };
 
-console.log(operate(3, 4, "*"));
-
 /*
  * Calculator Operation
  */
-
-// 4x4 Grid for the calculator buttons
-createGrid(4, ".lower");
-
-console.log(addTextToDisplay('hello', false))
-
-ans = 'hello';
-
-for (i = 0; i < 10; i++) {
-  numberButtonsUpdateDisplay(i)
-}
-
-addBtnCallAdd()
