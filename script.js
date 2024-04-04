70e7daa4818fdcd2f6937ccea63a5f237bd7f772
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-04-04 18:56:33 +0100

    Calculator numbers show on display

--- script.js
+++ script.js
@@ -2,78 +2,127 @@
 Create a Calculator
 */
 
-// Calculator operations
+/*
+ * Dom manipulation for On Screen Calculator
+ */
 
-const add = (a,b) => a + b
+// Calculator Buttons array
 
-const subtract = (a,b) => a - b
+const buttonsArr = [
+  "7",
+  "4",
+  "1",
+  "0",
+  "8",
+  "5",
+  "2",
+  ".",
+  "9",
+  "6",
+  "3",
+  "=",
+  "/",
+  "*",
+  "-",
+  "+",
+];
 
-const multiply = (a,b) => a * b
+function createGrid(gridSize, element) {
+  const grid = document.querySelector(element);
+  for (i = 1, arr = 0; i <= gridSize; i++) {
+    let col = document.createElement("div");
+    col.classList.add(`grid-column-${i}`);
+    for (j = 1; j <= gridSize; j++) {
+      let row = document.createElement("div");
+      row.classList.add(`grid-row-${j}`);
+      let btn = document.createElement("btn");
+      btn.classList.add(`btn-${buttonsArr[arr]}`)
+      btn.textContent = `${buttonsArr[arr]}`;
+      row.appendChild(btn);
+      arr++;
+      col.appendChild(row);
+    }
+    grid.appendChild(col);
+  }
+}
 
-const divide = (a, b) => a / b
+// 4x4 Grid for the calculator buttons
+createGrid(4, ".lower");
+
+// Function to display text on calculator
+displayTextSpan = document.querySelector(".display-text");
+let addTextToDisplay = function (text, update) {
+  if (update) {
+    return displayTextSpan.textContent += text;
+  }
+  return displayTextSpan.textContent = text;
+};
+
+console.log(addTextToDisplay('hello', false))
+
+ans = 'hello';
+
+// Function to populate display when buttons clicked
+
+let numberButtonsUpdateDisplay = function(btnNumber) {
+  let number = document.querySelector(`.btn-${btnNumber}`)
+
+  number.addEventListener('click', () => {
+    if (displayTextSpan.textContent == ans) {
+      displayValue = addTextToDisplay(`${btnNumber}`, false)
+      console.log(displayValue)
+    } else {
+      displayValue = addTextToDisplay(`${btnNumber}`, true)
+      console.log(displayValue)
+    }
+  })
+}
+
+for (i = 0; i < 10; i++) {
+  numberButtonsUpdateDisplay(i)
+}
+
+
+/*
+ * Functionality of Calculator
+ */
+
+// Variables for calculator
 
 let num1;
 let num2;
 let operator;
+var ans;
+var displayValue;
+
+// Basic operations function
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => {
+  if (b === 0) {
+    return "Error";
+  }
+
+  return a / b;
+};
 
 //Operate function
 
 const operate = (num1, num2, operator) => {
-    if (operator == '+') {
-        return add(num1, num2)
-    } else if (operator == '-') {
-        return subtract(num1, num2)
-    } else if(operator == '*') {
-        return multiply(num1, num2)
-    } else if(operator == '/') {
-        return divide(num1, num2)
-    }
-}
-
-console.log(operate(3, 4, '*'))
-
-// Calculator Buttons
-
-const buttonsArr = ['7', '4', '1', '0', '8', '5', '2', '.', '9', '6', '3', '=', '/', 'x', '-', '+']
-
-function createGrid(gridSize, element) {
-  const container = document.querySelector(element);
-  // Columns section - Create the number of columns needed for the grid
-  for (i = 1; i <= gridSize; i++) {
-    let div = document.createElement("div");
-    div.classList.add(`col${i}`);
-    container.appendChild(div);
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "*") {
+    return multiply(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
   }
+};
 
-  // Rows Section - For each column, make as many rows as there are columns
-  const columns = container.querySelectorAll("div");
-
-  // In each column, make as many rows as there are columns
-  for (i = 1, arr = 0; i <= columns.length; i++) {
-    // Select each column
-    let currentColumnDiv = container.querySelector(`.col${i}`);
-    console.log(`current column is col: ${i}`);
-    for (j = 1; j <= columns.length; j++) {
-      // Create as many rows as there are columns
-      //   console.log(`current row is row: ${j}`);
-      let newRowDiv = document.createElement("div");
-      newRowDiv.classList.add(`row${j}`);
-      //   console.log(currentColumn);
-      currentColumnDiv.appendChild(newRowDiv);
-      let btn = document.createElement("btn")
-      btn.classList.add(`${buttonsArr[arr]}`)
-      btn.textContent = `${buttonsArr[arr]}`
-      newRowDiv.appendChild(btn)
-      arr++;
-    }
-  }
-}
-
-createGrid(4, ".lower");
-
-// const
-
-for (i = 1; i < 10; i++) {
-    // create new button
-    //append button to
-}
+console.log(operate(3, 4, "*"));
